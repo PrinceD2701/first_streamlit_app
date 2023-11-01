7dc24709f9f88bfb0445853e0c238a13ce9d4858
PrinceD2701 <148716300+PrinceD2701@users.noreply.github.com>
2023-11-02 00:46:18 +0530

    Update my_first_app.py

--- my_first_app.py
+++ my_first_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.Title('This is my first app')
+streamlit.title('This is my first app')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
